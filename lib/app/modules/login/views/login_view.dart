e798b0d12f96c245b9a70b89feaa232dc1acd2c3
iKzuu <anangsetiaji30@gmail.com>
2024-01-31 14:13:41 +0700

    petugas commit

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -10,44 +10,71 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LoginView'),
+        title: const Text('Login NowLib'),
         centerTitle: true,
+        backgroundColor: Colors.black26,
       ),
-      body: Center(
-          child: Form(
-            key: controller.formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: controller.usernameController,
-                  decoration: InputDecoration(hintText: "Masukkan Username"),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "username tidak boleh kosong";
-                    }
-                    return null;
-                  },
-                ),
+      body: Container(
+        color: Colors.lightBlue,
+        child: Center(
+            child: Form(
+              key: controller.formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      controller: controller.usernameController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                          hintText: "Masukkan Username",
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "username tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
 
-                TextFormField(
-                  controller: controller.passwordController,
-                  decoration: InputDecoration(hintText: "Masukkan Password"),
-                  validator: (value) {
-                    if (value!.length <2) {
-                      return "password tidak boleh kosong";
-                    }
-                    return null;
-                  },
-                ),
-                Obx(() => controller.loading.value?
-                CircularProgressIndicator():
-                ElevatedButton(onPressed: () {
-                  controller.login();
-                }, child: Text("Login"))
-                )
-              ],
-            ),
-          )
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: controller.passwordController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                          hintText: "Masukkan Password",
+                          prefixIcon: Icon(Icons.lock_outline),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.length <2) {
+                          return "password tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Obx(() => controller.loading.value?
+                  CircularProgressIndicator():
+                  ElevatedButton(onPressed: () {
+                    controller.login();
+                  }, child: Text("Login"))
+                  )
+                ],
+              ),
+            )
+        ),
       ),
     );
   }
