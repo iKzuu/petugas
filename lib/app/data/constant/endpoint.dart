c696c08feea6b28844e6d82ea7198cd03f83fac9
iKzuu <anangsetiaji30@gmail.com>
2024-02-01 18:15:04 +0700

    petugas commit

--- lib/app/data/constant/endpoint.dart
+++
@@ -1,8 +0,0 @@
-class Endpoint {
-  static const String baseUrlApi = "https://api-perpustakaan.abadjayasenantiasa.com/api/v1/petugas/";
-
-
-  static const String login = "${baseUrlApi}login";
-  static const String book = "${baseUrlApi}book";
-  static const String pinjam = "${baseUrlApi}pinjam";
-}
